929d5c263da7b33f71b025db7e7201fb5bb57878
Selyatin <selyatinismet@gmail.com>
2021-02-26 04:23:38 +0200

    Fix drunk code lol

--- src/config.rs
+++ src/config.rs
@@ -9,37 +9,23 @@ pub struct Config {
 
 impl Config {
     pub fn new<S: AsRef<str>>(path: S) -> Result<Config, (Config, String)> {
-        let json_data_as_str = std::fs::read_to_string(path.as_ref());
 
-        if let Err(err) = json_data_as_str {
-            return Err((Config::default(), err.to_string()));
-        }
+        let json_data_as_str = match std::fs::read_to_string(path.as_ref()){
+            Ok(data) => data,
+            Err(err) => return Err((Config::default(), err.to_string()))
+        };
 
-        let json_data = serde_json::from_str(&json_data_as_str.unwrap()[..]);
+        let mut config: Config = match serde_json::from_str(&json_data_as_str){
+            Ok(config) => config,
+            Err(err) => return Err((Config::default(), err.to_string()))
+        };
 
-        if let Err(err) = json_data {
-            return Err((Config::default(), err.to_string()));
+        
+        if config.history_path.is_empty() {
+            config.history_path = format!("{}/.ephosh_history", std::env::var("HOME").unwrap());
         }
         
-        let json_data: Config = json_data.unwrap();
-
-        let history_path = json_data.history_path.to_string();
-
-        let history_path = match history_path.is_empty() {
-            true => format!("{}/.ephosh_history", std::env::var("HOME").unwrap()),
-            false => history_path,
-        };
-
-        let history_size = match json_data.history_size.to_string().is_empty() {
-            true => 1000,
-            false => json_data.history_size,
-        };
-
-        Ok(Config {
-            max_outputs: json_data.max_outputs,
-            history_path,
-            history_size,
-        })
+        Ok(config)
     }
 }
 impl Default for Config {
