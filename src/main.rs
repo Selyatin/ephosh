3418d55159a2b80e54fc47c5daa0391379b0deff
Selyatin <selyatinismet@gmail.com>
2021-02-27 11:58:18 +0200

    Shell struct moved into it's own file

--- src/main.rs
+++ src/main.rs
@@ -8,87 +8,24 @@ use tui::{
     style::{Style, Color, Modifier},
     text::{Spans, Span},
     widgets::{Block, Borders, Paragraph},
-    layout::{Layout, Constraint, Direction, Rect},
+    layout::{Layout, Constraint, Direction},
 };
 use std::{
     io::{self, Write},
     env,
-    path::Path,
-    fs::{File, OpenOptions},
     cmp::Ordering,
 };
 
 mod ui;
 mod utils;
 mod event;
+mod shell;
 mod non_blocking;
 mod inbuilt;
 mod config;
-mod cursor;
 
 use ui::Pane;
-use cursor::Cursor;
-use config::Config;
-
-struct Shell {
-    pub username: String,
-    pub current_dir: String,
-    pub panes: Vec<Pane>,
-    pub error: String,
-    pub input: String,
-    pub config: Config,
-    pub history: File,
-    pub cursor: Cursor,
-    pub chunks: Vec<Rect>
-}
-impl Default for Shell {
-    fn default() -> Self {
-        let home_var = match env::var("HOME") {
-            Ok(var) => var,
-            Err(_) => panic!("Error: Couldn't get HOME var")
-        };
-
-        let config_path = format!("{}/.config/ephosh/ephosh.json", home_var);
-
-        let config = match Path::new(&config_path).is_file() {
-            true => {
-                let config = Config::new(config_path);
-                match config {
-                    Ok(config) => config,
-                    Err(err) => {
-                        err.0
-                    }
-                }
-            },
-            false => Config::default()
-        };
-        let current_dir = env::current_dir().unwrap().to_str().unwrap().to_owned();
-
-        let username = env::var("USER").unwrap().to_owned();
-
-        let history = OpenOptions::new()
-            .write(true)
-            .read(true)
-            .open(&config.history_path);
-
-        let history = if let Err(_) = history {
-            File::create(&config.history_path).unwrap()
-        } else {
-            history.unwrap()
-        };
-        Self {
-            username,
-            current_dir,
-            config,
-            error: "".to_owned(),
-            input: "".to_owned(),
-            panes: vec![],
-            history,
-            cursor: Cursor::new(1, 1),
-            chunks: vec![]
-        }
-    }
-}
+use shell::Shell;
 
 fn main() -> Result<(), io::Error> {
 
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,3 +1,5 @@
 mod pane;
+mod cursor;
 
 pub use pane::Pane;
+pub use cursor::Cursor;
